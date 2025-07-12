1c3dd9f343b42e688eb575757eb2d16245ccfef1
hale0904 <leduyha2809@gmail.com>
2025-07-12 11:11:26 +0700

    chore(init): Setup project folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { HomeComponent } from './pages/p-personnel/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HomeComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
+    CommonModule, 
     AppRoutingModule
   ],
   providers: [],
