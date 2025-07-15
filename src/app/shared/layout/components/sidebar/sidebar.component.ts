7df69b7c860e49a6d6c97ce82169225e6715773e
hale0904 <leduyha2809@gmail.com>
2025-07-15 13:29:17 +0700

    feat(sidebar): update nav

--- src/app/shared/layout/components/sidebar/sidebar.component.ts
+++ src/app/shared/layout/components/sidebar/sidebar.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  items = [
+    {imgLogo: 'assets/images/logo/logo.png', descrip: 'Logo HACHI HACHI', dropdown: 'fa-caret-down'}
+  ]
 
+  navs = [
+    {icon: 'fa-list-check', title: 'Đánh giá nhân sự', dropdown: 'fa-caret-down'},
+    {icon: 'fa-list-check', title: 'Quản lý sản phẩm', dropdown: 'fa-caret-down'}
+  ]
 }
